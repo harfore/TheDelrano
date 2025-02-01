4db448376c5735b36e8ceb667a94843f0f0b3418
harfore <remiharfoush@gmail.com>
2025-02-01 12:59:05 +0100

    setup: initial frontend directory structure (WIP)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>The Delrano is currently undergoing renovation. Stay tuned for a spectacular opening night.</p>
+    </>
+  )
+}
+
+export default App
